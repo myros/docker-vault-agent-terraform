1f9fdee909e63ff1b777f4bfd667b6d17e185dbf
Myros <myros.net@gmail.com>
2023-04-12 16:03:57 -0400

    Init

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
+
+provider "vault" {
+  address = "http://localhost:8200"
+  token = "root"
+}
